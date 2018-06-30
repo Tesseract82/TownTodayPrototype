2a2c3a0f72643d9ee3bd0132c5a4b4e524258e83
Berin <be.selimotic72@gmail.com>
2018-06-29 20:21:30 -0700

    first commit

---
+++ Images/index.js
@@ -0,0 +1,10 @@
+export images = {
+    nav_top_left: require('./settingsIcon.png'),
+    //nat_top_center: require('./townToday.png'),
+    nav_top_right: require('./searchIcon.png'),
+    nav_bottom_1: require('./addEventIcon.png'),
+    nav_bottom_2: require('./searchIcon.png'),
+    nav_bottom_3: require('./homeIcon.png'),
+    nav_bottom_4: require('./bookmarkIcon.png'),
+    nav_bottom_5: require('./locationIcon.png')
+};
